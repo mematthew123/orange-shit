dfed7b140a65d3260f766f0bc55e83c972eaa31e
Matthew R <mematthew123@gmail.com>
2025-07-21 08:22:48 -0600

    Refactor Home component to remove duplicate EpsteinBirthdayQuoteSection and adjust component order

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from '@/components/Hero';
 import IvankaQuoteSection from '@/components/IvankaQuoteSection';
 import QuoteSection from '@/components/QuoteSection';
 import TestimonyQuoteSection from '@/components/TestimonyQuoteSection';
-import Timeline from '@/components/Timeline';
 import EpsteinBirthdayQuoteSection from '@/components/EpsteinBirthdayQuoteSection';
 import LineTimeline from '@/components/LineTimeline';
 
@@ -10,12 +9,11 @@ export default function Home() {
     return (
         <>
             <Hero />
+            <EpsteinBirthdayQuoteSection />
             <QuoteSection />
             <LineTimeline />
-            <Timeline />
             <IvankaQuoteSection />
             <TestimonyQuoteSection />
-            <EpsteinBirthdayQuoteSection />
         </>
     );
 }
